147863cec0a92f777911310c1b4a5047e0e70699
Jaicy <jaicychoi@gmail.com>
2020-07-03 16:00:52 +0900

    portfolio page modified

--- js/common.js
+++ js/common.js
@@ -1,11 +1,44 @@
 let getNodeindex = elm => [...elm.parentNode.children].indexOf(elm);
 
+const logo = document.querySelector('#logo');
 const menu_btn = document.querySelector('#menu_btn');
 const nav = document.querySelector('nav');
 const li = document.querySelectorAll('li');
 const section = document.querySelectorAll('section');
+const cursor = document.querySelector('#cursor');
 
 let menu_click = false;
+
+if( window.location.href.indexOf('index') > 0 )
+    cursor.classList.add('hide');
+
+document.addEventListener('mousemove', (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+
+    cursor.style.left = x + 'px';
+    cursor.style.top = y + 'px';
+});
+
+nav.parentElement.addEventListener('mouseenter', () => {
+    cursor.classList.remove('hide');
+});
+if( window.location.href.indexOf('index') > 0 ){
+    nav.parentElement.addEventListener('mouseleave', () => {
+        cursor.classList.add('hide');
+    });
+}
+
+logo.addEventListener('mouseenter', () => { cursor.classList.add('select'); });
+logo.addEventListener('mouseleave', () => {  cursor.classList.remove('select'); });
+menu_btn.addEventListener('mouseenter', () => { cursor.classList.add('select'); });
+menu_btn.addEventListener('mouseleave', () => { cursor.classList.remove('select'); });
+li.forEach(li => li.addEventListener('mouseenter', () => {
+    cursor.classList.add('select');
+}));
+li.forEach(li => li.addEventListener('mouseleave', () => {
+    cursor.classList.remove('select');
+}));
 
 menu_btn.addEventListener('click', function(){
     if( !menu_click ){
@@ -14,6 +47,7 @@ menu_btn.addEventListener('click', function(){
         nav.classList.add('show');
         section.forEach(section => section.classList.add('blur'));
         li.forEach(li => li.classList.add('pop'));
+        cursor.classList.remove('hide');
     }
     else{
         menu_btn.classList.remove('click');
@@ -21,5 +55,7 @@ menu_btn.addEventListener('click', function(){
         nav.classList.remove('show');
         section.forEach(section => section.classList.remove('blur'));
         li.forEach(li => li.classList.remove('pop'));
+        if( window.location.href.indexOf('index') > 0 )
+            cursor.classList.add('hide');
     }
 });
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,15 +1,59 @@
 const img = document.querySelectorAll('.preview_img');
 const preview = document.querySelectorAll('.preview_img p');
 
-img.forEach(img => img.addEventListener('mouseenter', () => {
+const degreeToRadian = (angle) => {
+    return angle * (Math.PI / 180);
+};
+const radius = 150;
+const circle_text = cursor.innerText;
+const characters = circle_text.split('');
+cursor.innerText = null;
+
+const startAngle = -90;
+const endAngle = -50;
+const angleRange = endAngle - startAngle;
+
+const deltaAngle = angleRange / characters.length;
+let currentAngle = startAngle;
+
+characters.forEach((char, index) => {
+    const charElement = document.createElement('span');
+    charElement.classList.add('hide');
+    charElement.innerText = char;
+    const xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
+    const yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
+
+    const transform = `translate(${xPos}px, ${yPos}px)`;
+    const rotate = `rotate(${index * deltaAngle}deg)`;
+    charElement.style.transform = `${transform} ${rotate}`;
+
+    currentAngle += deltaAngle;
+    cursor.appendChild(charElement);
+});
+
+img.forEach(img => img.childNodes[1].addEventListener('mouseenter', () => {
         img.childNodes[1].classList.add('color');
+        cursor.classList.add('point');
+        cursor.childNodes.forEach(span => span.classList.remove('hide'));
         preview[getNodeindex(img)].classList.add('show');
+
+        if( document.body.classList.value === 'dark_mode')
+            img.childNodes[3].classList.add('dark_mode');
+        else
+            img.childNodes[3].classList.remove('dark_mode');
     }
 ));
 img.forEach(img => img.addEventListener('mouseleave', () => {
         img.childNodes[1].classList.remove('color');
+        cursor.classList.remove('point');
+        cursor.childNodes.forEach(span => span.classList.add('hide'));
         preview[getNodeindex(img)].classList.remove('show');
     }
 ));
 
-//mofity mouse over effect
+const mode = document.querySelector('#mode');
+
+mode.addEventListener('change', () => {
+    document.body.classList.toggle('dark_mode');
+    document.querySelector('.main_title').classList.toggle('dark_mode');
+});
