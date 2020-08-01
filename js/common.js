661baf9b3d44f911615ae7790d5953c41f928570
jaicychoi <jaicychoi@gamil.com>
2020-08-02 04:45:53 +0900

    menu condition error refixed

--- js/common.js
+++ js/common.js
@@ -169,7 +169,7 @@ menu_btn.addEventListener('click', () => {
         menu_click = true;
         nav.classList.add('show');
         content.classList.add('blur');
-        if( window.location.href.indexOf('index') === -1 && window.location.href.length > 38 )
+        if( document.title !== 'CYJ' && window.location.href.length > 38 )
             document.querySelector('.label').classList.add('blur');
         language_option.classList.add('blur');
         li.forEach(li => li.classList.add('pop'));
@@ -186,7 +186,7 @@ menu_btn.addEventListener('click', () => {
         menu_btn.classList.remove('click');
         menu_click = false;
         nav.classList.remove('show');
-        if( window.location.href.indexOf('index') === -1 && window.location.href.length > 38 )
+        if( document.title !== 'CYJ' && window.location.href.length > 38 )
             document.querySelector('.label').classList.remove('blur');
         language_option.classList.remove('blur');
         content.classList.remove('blur');
