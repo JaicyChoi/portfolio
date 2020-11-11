7783393d91dcf3301ecc0b258dc3ede0512c8716
jaicychoi <jaicychoi@gamil.com>
2020-11-11 12:12:21 +0900

    rollback

--- js/main.js
+++ js/main.js
@@ -6,8 +6,7 @@ const parallax = document.querySelector('.parallax');
 const prev_btn = document.querySelector('#prev_btn');
 const next_btn = document.querySelector('#next_btn');
 const typing_text = document.querySelectorAll('.main_subtitle p');
-let text = null;
-if( isIE === false ) text = document.querySelector('svg text');
+const text = document.querySelector('svg text');
 const vanilla = document.querySelector('#vanilla');
 let index = 0;
 
@@ -16,7 +15,7 @@ for( let i = 0 ; i < section.length; i++ ){
     else section[i].style.cssText = 'background: url(images/main_' + i + '.jpg) no-repeat center; background-size: cover; background-attachment: fixed;';
 }
 
-setTimeout( function(){ main_visual_text.style.cssText = 'transition-delay: 0s;' }, 4000);
+setTimeout( () => { main_visual_text.style.cssText = 'transition-delay: 0s;' }, 4000);
 
 const main_button = document.querySelectorAll('.main_button');
 let main_text, vanilla_text;
@@ -36,12 +35,12 @@ function text_setting(){
     if(  localStorage.getItem('hash') === '#kor' ){
         vanilla.classList.remove('eng');
         main_visual_text.classList.add('kor');
-        main_button.forEach(function(button){button.classList.add('kor')});
+        main_button.forEach(button => button.classList.add('kor'));
     }
     else{
         vanilla.classList.add('eng');
         main_visual_text.classList.remove('kor');
-        main_button.forEach(function(button){button.classList.remove('kor')});
+        main_button.forEach(button => button.classList.remove('kor'));
     }
 
     vanilla.innerText = vanilla_text;
@@ -50,22 +49,22 @@ function text_setting(){
         main_button[i].innerText = main_text[i+1];
 }
 
-eng.addEventListener('click', function(){
+eng.addEventListener('click', () => {
     vanilla_text = VANILLA.eng;
     main_text = MAIN_TEXT.eng;
     main_visual_text.style.transition = 'none';
     text_setting();
-    setTimeout( function(){main_visual_text.style.transition = 'all 2s ease';}, 1000 );
+    setTimeout( () => {main_visual_text.style.transition = 'all 2s ease';}, 1000 );
 });
-kor.addEventListener('click', function(){
+kor.addEventListener('click', () => {
     vanilla_text = VANILLA.kor;
     main_text = MAIN_TEXT.kor;
     main_visual_text.style.transition = 'none';
     text_setting();
-    setTimeout( function(){main_visual_text.style.transition = 'all 2s ease';}, 1000 );
+    setTimeout( () => {main_visual_text.style.transition = 'all 2s ease';}, 1000 );
 });
 
-const toggleText = function(index, state){
+const toggleText = (index, state) => {
     if( index === 0 ) return;
     if (state === 'show')
       section[index].querySelector('.main_text').classList.add('show');
@@ -73,28 +72,28 @@ const toggleText = function(index, state){
       section[index].querySelector('.main_text').classList.remove('show');
   };
   
-  const typingText = function(index, state){
+  const typingText = (index, state) => {
       if( state === 'show')
         typing_text[index].style.cssText = 'animation: typing 3.5s steps(' + typing_text[index].innerText.length + ', end) forwards, blink 1s infinite;';
     else
         typing_text[index].style.cssText = 'animation: none;';
   };
 
-setTimeout( function(){
-    if( isIE === false ) text.classList.add('visible');
+setTimeout( () => {
+    text.classList.add('visible');
     main_visual_text.classList.add('show');
 }, 2100);
 
-prev_btn.parentElement.addEventListener('mouseenter', function(){ cursor.classList.remove('hide'); cursor.classList.add('select'); });
-prev_btn.parentElement.addEventListener('mouseleave', function(){ cursor.classList.add('hide'); cursor.classList.remove('select'); });
+prev_btn.parentElement.addEventListener('mouseenter', () => { cursor.classList.remove('hide'); cursor.classList.add('select'); });
+prev_btn.parentElement.addEventListener('mouseleave', () => { cursor.classList.add('hide'); cursor.classList.remove('select'); });
 
-prev_btn.addEventListener('click', function(){
+prev_btn.addEventListener('click', () => {
     if( index < 1 ) return;
 
     toggleText(index, 'hide');
     index--;
     section[index].scrollIntoView({behavior: 'smooth'});
-})
+});
 next_btn.addEventListener('click', () => {
     if( index > 2 ) return;
 
@@ -103,7 +102,7 @@ next_btn.addEventListener('click', () => {
     section[index].scrollIntoView({behavior: 'smooth'});
 });
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', () => {
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset * 0.7 + 'px';
      
@@ -127,7 +126,7 @@ window.addEventListener('scroll', function(){
         typingText(0, 'show');
         index = 1;
         main_visual_text.classList.remove('show');
-        if( isIE === false ) text.classList.remove('visible');
+        text.classList.remove('visible');
         prev_btn.classList.add('visible');
     }
     else if( offset < section[0].offsetHeight * 0.5 ){
@@ -135,18 +134,18 @@ window.addEventListener('scroll', function(){
         toggleText(1, 'hide'); index = 0;
         if( offset < section[0].offsetHeight * 0.3 ){
             main_visual_text.classList.add('show');
-            if( isIE === false ) text.classList.add('visible');
+            text.classList.add('visible');
             prev_btn.classList.remove('visible');
         }
     }
     if( offset === 0 ) section[0].style.cssText = 'background: url(images/main_0.jpg) no-repeat center; background-size: contain; background-color: black;';
 });
 
-main_button.forEach(function(button){button.addEventListener('mouseenter', function(){
+main_button.forEach(button => button.addEventListener('mouseenter', () => {
     cursor.classList.remove('hide');
     cursor.classList.add('select');
-})});
-main_button.forEach(function(button){button.addEventListener('mouseleave', function(){
+}));
+main_button.forEach(button => button.addEventListener('mouseleave', () => {
     cursor.classList.add('hide');
     cursor.classList.remove('select');
-})});
+}));
